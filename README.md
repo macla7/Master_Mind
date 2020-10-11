0d417ce0901cf368763310628ea6a6888f4c1c00
macla7 <mitchclarkmapie@hotmail.com>
2020-10-12 07:50:37 +1100

    player assingment and README

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Master_Mind
+Going to try and follow along with this class flow type method that 'Nicholas Berube' did in his answer. Thought this was ingenius so congrats to him lol.
---
+++ player.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Asking Human for role and assinging
+class Player
+  def initialize
+    @human = ''
+    @comp = ''
+    ask_human
+  end
+
+  def ask_human
+    puts "\nWould you like to play as the 'CodeBreaker' or the 'CodeMaker'?"
+    @human = gets.chomp
+    human_role
+  end
+
+  def human_role
+    if @human == 'CodeBreaker'
+      @comp == 'CodeMaker'
+    elsif @human == 'CodeMaker'
+      @comp == 'CodeBreaker'
+    else
+      puts "\nPlease enter in one of the two options:"
+      ask_human
+    end
+    # Setup.new(@human, @comp)
+  end
+end
+
+Player.new
